ce704dd46a09ac9a82d1c796d50f7a25da909fe7
bridgetgibbons <bvgibbons@gmail.com>
2020-04-13 19:39:27 -0700

    roughing in another idea for the function

--- yield_function.R
+++ yield_function.R
@@ -7,3 +7,28 @@ almond_yield = function(x1 = -0.015, x2 = -0.0046, x3 = -0.07, x4 = 0.0043, inte
   return(yield_anomaly)
   
 }
+
+
+##############
+
+
+almond_yield_take2 = function(x1 = -0.015, x2 = -0.0046, x3 = -0.07, x4 = 0.0043, interceptt = 0.28, climate){
+  
+  # sort for the temperature and precipitation data based on month
+  
+  if(month="1"){
+    climate$jan_precip = climate$precip
+  }
+  if(month="2"){
+    climate$feb_mintemp = climate$meantmin
+  }
+  
+  climate$yield_anomaly = (x1*climate$feb_mintemp) + (x2*(climate$feb_mintemp^2)) + (x3*(climate$jan_precip^2)) + intercept
+  
+  min_anomaly = min(climate$yield_anomaly)
+  
+  max_anomaly = max(climate$yield_anomaly)
+  
+  return(list(annual_anomaly = climate[,c("year", "yield_anomaly")], max = max_anomaly, min = min_anomaly))
+  
+}
