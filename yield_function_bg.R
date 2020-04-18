86d44c9ea8ef52674792ec4a5c243fe383094f39
bridgetgibbons <bvgibbons@gmail.com>
2020-04-18 11:14:19 -0700

    added comments, working on #5

--- yield_function_bg.R
+++ yield_function_bg.R
@@ -1,8 +1,15 @@
 yield_function_bg = function(x1 = -0.015, x2 = -0.0046, x3 = -0.07, x4 = 0.0043, intercept = 0.28, climate){
   
+  # organize the climate data by month and year
+  # create summary of the mean max and min temperatures, and the mean precipitation by month
+  
   clim_month = climate %>% 
     group_by(month, year) %>% 
     summarize(meantmax = mean(tmax_c), meantmin = mean(tmin_c), precip=mean(precip)) 
+  
+  # filter for months of interest for almonds
+  # january - precipitation
+  # february - minimum temperature
   
   jan_precip <- clim_month %>% 
     dplyr::filter(month == 1)
@@ -10,16 +17,26 @@ yield_function_bg = function(x1 = -0.015, x2 = -0.0046, x3 = -0.07, x4 = 0.0043,
   feb_temp <- clim_month %>% 
     dplyr::filter(month == 2)
   
+  # create a column to store the yield anomaly values in one of the existing data frames that has rows for each year of interest
+  
   output_df <- as.data.frame(feb_temp) %>% mutate(YA = NA)
+  
+  # use a for loop to have the function run for each row [i] using the columns of interest, and store the output in the YA column of the output data frame
   
   for(i in 1:nrow(output_df)){
     output_df$YA[i] <- x1*feb_temp$meantmin[i] + x2*feb_temp$meantmin[i]^2 + x3*jan_precip$precip[i] + x4*jan_precip$precip[i]^2 + intercept
   }
   
+  # pull out the year and yield anomaly to include in the function output
+  
   yield_anomaly_year <- dplyr::select(output_df, c(year, YA))
+  
+  # calculate the minimum and maximum anomaly values from the YA column, give a title so they can be called in the list
   
   min_value <- yield_anomaly_year[yield_anomaly_year$YA == min(yield_anomaly_year$YA),]
   max_value <- yield_anomaly_year[yield_anomaly_year$YA == max(yield_anomaly_year$YA),]
+  
+  # set the function to return a list of the values of interest
   
   return(list(yield_anomaly_year, min_value, max_value))
   
